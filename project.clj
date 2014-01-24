87741edf0873c31d6ffda355370eb7c0e27fcf49
Stephen Sloan <steve@stevesloan.com>
2014-01-23 20:46:46 -0800

    Mostly irrelevant project.clj additions

--- project.clj
+++ project.clj
@@ -1,6 +1,11 @@
 (defproject clojure-dojo "0.1.0-SNAPSHOT"
   :description "Personal clojure dojo for Stephen Sloan"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/polygloton/clojure-dojo"
+  :license {:name "MIT"
+            :url "https://raw2.github.com/polygloton/clojure-dojo/master/LICENSE.txt"
+            :distribution "manual"}
+  :min-lein-version "2.0.0"
+  :pendantic? :warn
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.logic "0.7.5"]]
   :test-selectors {:default (constantly false)
